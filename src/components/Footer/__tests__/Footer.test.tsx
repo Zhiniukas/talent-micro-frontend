d4b59d2c8bd9cc61f11bc6976435849d46687f43
Zhiniukas <zhiniukas@gmail.com>
2023-03-20 19:48:26 +0200

    ABC-167: Footer with current year

---
+++ src/components/Footer/__tests__/Footer.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import { Footer } from "../Footer";
+import renderer from "react-test-renderer";
+
+describe("Footer", () => {
+  it("should match snapshot", () => {
+    const tree = renderer.create(<Footer />);
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("should render current year", () => {
+    render(<Footer />);
+    const currentYearElement = screen.getByLabelText("current year");
+
+    expect(currentYearElement).toBeVisible();
+    expect(currentYearElement.textContent).toBe(`${new Date().getFullYear()}`);
+  });
+});
